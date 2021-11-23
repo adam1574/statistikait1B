226d6cdc0697d84deb192399440dbbb290a969e4
Sivera.Adam <sivera.adam@seznam.cz>
2021-11-23 11:34:18 +0100

    skoro gotovo

--- statistikait1B/Program.cs
+++ statistikait1B/Program.cs
@@ -6,7 +6,41 @@ namespace statistikait1B
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConsoleKeyInfo keyinfo;
+            string PZ = "0123456789";  //PZ = povolené znaky 
+            string text = "";
+            int soucet = 0;
+            int pocet = 0;
+            do
+            {
+                keyinfo = Console.ReadKey(true);
+                if (PZ.Contains(keyinfo.KeyChar))
+                {
+                    //zde se program dostane při stisknutí čísla
+                    text = text + keyinfo.KeyChar; //text += keyinfo.KeyChar;
+                    Console.Write(keyinfo.KeyChar);
+
+
+                }
+                else if (keyinfo.Key == ConsoleKey.Enter)
+                {
+                    if(text != "")
+                    {
+                        soucet += int.Parse(text);
+                        pocet++;
+                    }
+                    Console.WriteLine();
+                    text = "";
+
+                }
+                else if (keyinfo.Key == ConsoleKey.Backspace && text.Length > 0)
+                {
+                    Console.Write("\b \b");
+                    text = text.Substring(0, text.Length - 1);
+
+                }
+            }   while (keyinfo.Key != ConsoleKey.Escape);
+
         }
     }
 }
